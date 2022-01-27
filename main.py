00c65afb94d26dc76109e0578dde1b6ee11b2b48
Jörg Rüggberg <joerg.rueggeberg@googlemail.com>
2022-01-27 15:08:39 +0100

    added messageboxes

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 from tkinter import *
+from tkinter import messagebox
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate():
+    pass
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-def generate():
+def add_pass():
     data_website = entry_website.get()
     data_username = entry_username.get()
     data_password = entry_password.get()
-    with open("data.txt", "a") as f:
-        f.write(f"{data_website}, {data_username}, {data_password}\n")
-    entry_website.delete(0, END)
-    entry_website.insert(END, string="https://www.")
-    entry_username.delete(0, END)
-    entry_password.delete(0, END)
+
+    if len(data_website) == 0 or len(data_username) == 0 or len(data_password) == 0:
+        messagebox.showwarning(title="Error - Empty fields", message="Please fill out all required fields!")
+    else:
+        print("nicht leer")
+        confirm = messagebox.askokcancel(title=data_website, message=f"These are the details entered: \n\n"
+                                                                     f"Username: {data_username}\n"
+                                                                     f"Password: {data_password}\n\n"
+                                                                     f"Do you want to save these information?")
+        if confirm:
+            with open("data.txt", "a") as f:
+                f.write(f"{data_website}, {data_username}, {data_password}\n")
+
+            entry_website.delete(0, END)
+            entry_website.insert(0, string="https://www.")
+            entry_username.delete(0, END)
+            entry_password.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -50,7 +65,7 @@ generate_pass = Button(text="Generate Password", command=generate)
 generate_pass.grid(column=2, row=3, sticky="ew")
 
 # calls action() when pressed
-add = Button(text="Add", command=generate, width=34)
+add = Button(text="Add", command=add_pass, width=34)
 add.grid(column=1, row=4, columnspan=2, sticky="ew")
 
 window.mainloop()
